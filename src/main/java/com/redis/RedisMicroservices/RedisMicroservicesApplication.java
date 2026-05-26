b87d09b72fad5304f09fcaccec2bd48dbc00845a
BGAJARDO <braulio.andres.9@gmail.com>
2026-05-26 13:05:34 -0400

    Upload project

---
+++ src/main/java/com/redis/RedisMicroservices/RedisMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.redis.RedisMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisMicroservicesApplication.class, args);
+	}
+
+}
